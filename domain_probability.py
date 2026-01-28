284e16942d5606e0c33cc913548d9d1be3532a4e
almire <almmiree@gmail.com>
2026-01-28 14:38:07 -0500

    xxx

--- domain_probability.py
+++ domain_probability.py
@@ -6,6 +6,8 @@ import datetime
 #this is a test
 from scapy.layers.dns import DNSQR, DNSRR
 #amother test
+#thrid test
+
 # Define the length of each time window (in seconds)
 window_length = 3600  # 1 hour
 
